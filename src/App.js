33139dee8353acb3ad19f14545cdf5e0c8e52403
Richard00923 <vasyakov6@gmail.com>
2022-10-03 12:45:45 +0300

    є

--- src/App.js
+++ src/App.js
@@ -1,16 +1,1 @@
 import logo from './logo.svg';
-import './App.css';
-import Simpson from "./componetnts/Simpson";
-import RikAndMorty from "./componetnts/rikAndMortyDate";
-
-function App() {
-  return (<div>
-        <Simpson/>
-        <RikAndMorty/>
-      </div>
-
-
-  );
-}
-
-export default App;
