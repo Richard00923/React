9786c7929381fe7a0a1f8f8faa999931ec159707
Richard00923 <vasyakov6@gmail.com>
2022-10-04 13:03:09 +0300

    є

--- src/componetnts/User.js
+++ src/componetnts/User.js
@@ -1,11 +1,28 @@
-// import {useEffect} from "react";
-// import {getUserAxios, getUsersAxios} from "./Users";
-//
-//
-// export default function Test (){
-//     useEffect(()=>{
-//         getUserAxios().then(value => console.log(value.data.map(console.log(value.data))))
-//     })
-// }
+import axios from "axios";
+import {useEffect, useState} from "react";
+
+export default function Hm2() {
+    let axiosinstant =axios.create({baseURL:'https://jsonplaceholder.typicode.com/users/'})
+
+    let [character, setCharacter] = useState([])
+    let [user, setUser] =  useState(null);
+    const lift = (id)=>{
+        setUser(id)
+    }
+    useEffect(()=>{
+        axiosinstant.get().then(users=>{setCharacter(users.data)})
 
 
+
+    },[])
+
+
+    return(<div>
+        <div>{user?.id}</div>
+        <div>{user?.name}</div>
+        <div>{user?.username}</div>
+        <div>{user?.email}</div>
+        {character.map(value => (<User user= {value} key={value.id} lift ={lift}/>))}
+
+    </div>)
+}
